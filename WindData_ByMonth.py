77186eb577629b3ccc42301a61cc21493bd92efe
Mohammed Almotawa <127317433+MohammedAlmotawa@users.noreply.github.com>
2023-03-11 16:06:15 +0300

    Update and rename AAA_CO_Wind.py to WindData_ByMonth.py

--- WindData_ByMonth.py
+++ WindData_ByMonth.py
@@ -3,19 +3,19 @@ from re import X
 import openpyxl
 
 #The source Data
-WORKBOOK1 = openpyxl.load_workbook('AACO_DividedByMonth.xlsx')
+WORKBOOK1 = openpyxl.load_workbook('COData_DividedByMonth.xlsx')
 SHEET1 = WORKBOOK1['1']
-WORKBOOK2 = openpyxl.load_workbook('Abha_ByMonth.xlsx')
+WORKBOOK2 = openpyxl.load_workbook('Abha.xlsx')
 SHEET2 = WORKBOOK2['1']
-WORKBOOK3 = openpyxl.load_workbook('Alahsa_ByMonth.xlsx')
+WORKBOOK3 = openpyxl.load_workbook('Alahsa.xlsx')
 SHEET3 = WORKBOOK3['1']
-WORKBOOK4 = openpyxl.load_workbook('Albaha_ByMonth.xlsx')
+WORKBOOK4 = openpyxl.load_workbook('Albaha.xlsx')
 SHEET4 = WORKBOOK4['1']
-WORKBOOK5 = openpyxl.load_workbook('Albaha_ByMonth.xlsx')
+WORKBOOK5 = openpyxl.load_workbook('Albaha.xlsx')
 SHEET5 = WORKBOOK5['1']
-WORKBOOK6 = openpyxl.load_workbook('Aljouf_ByMonth.xlsx')
+WORKBOOK6 = openpyxl.load_workbook('Aljouf.xlsx')
 SHEET6 = WORKBOOK6['1']
-WORKBOOK7 = openpyxl.load_workbook('Arar_ByMonth.xlsx')
+WORKBOOK7 = openpyxl.load_workbook('Arar.xlsx')
 SHEET7 = WORKBOOK7['1']
 
 #NE: new excel
@@ -29,7 +29,7 @@ ListMonth = ["%.2d" % i for i in range(1,13)]
 for D in range (1,32):
     NE.create_sheet(str(D))
 
-#Add the same Headings as the original file for each sheeat
+#Addin the same Headings as the original file for each sheeat
 #for d in range(1,32):
 #    SW1 = NE.active
 #    SW1 = NE[str(d)]
@@ -37,7 +37,7 @@ for D in range (1,32):
 #    SW1.append([ SHEET2 ['A1'].value , SHEET2 ['B1'].value , SHEET2 ['C1'].value , SHEET2 ['D1'].value , SHEET2 ['E1'].value , SHEET2 ['F1'].value , SHEET2 ['G1'].value , SHEET2 ['H1'].value , SHEET2 ['I1'].value , SHEET2 ['J1'].value, SHEET2 ['K1'].value , SHEET2 ['L1'].value , SHEET2 ['M1'].value , SHEET2 ['N1'].value , SHEET2 ['O1'].value , SHEET2 ['P1'].value , SHEET2 ['Q1'].value , SHEET2 ['R1'].value , SHEET2 ['S1'].value , SHEET2 ['T1'].value, SHEET2 ['U1'].value , SHEET2 ['V1'].value , SHEET2 ['W1'].value , SHEET2 ['X1'].value , SHEET2 ['Y1'].value , SHEET2 ['Z1'].value , SHEET2 ['AA1'].value , SHEET2 ['AB1'].value , SHEET2 ['AC1'].value , SHEET2 ['AD1'].value, SHEET2 ['AE1'].value , SHEET2 ['AF1'].value , SHEET2 ['AG1'].value , SHEET2 ['AH1'].value , SHEET2 ['AI1'].value ])
 
 
-#Move every measurment based on the measurment month
+#Moving every measurment based on the measurment month
 for row in range (2,30): #CO #527
     cell = 'A' + str (row)
     X = str(SHEET1[cell].value)
@@ -57,11 +57,11 @@ for row in range (2,30): #CO #527
                 continue
 
 
-#Delete the first sheet
+#Deleting the first sheet
 del NE['Sheet']
 
 #Save the file        
-NE.save ('TEST.xlsx')
+NE.save ('Wind.xlsx')
 
 
 
