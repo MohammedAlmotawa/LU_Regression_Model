1c382689a6518a691139cd217bc3ea5feb1085d7
Mohammed Almotawa <127317433+MohammedAlmotawa@users.noreply.github.com>
2023-03-11 16:29:40 +0300

    Update CO_Step2_DeleteNULLEmptyRows.py

--- CO_Step2_DeleteNULLEmptyRows.py
+++ CO_Step2_DeleteNULLEmptyRows.py
@@ -1,4 +1,4 @@
-#Defining (back to (Excel_CO2019) file for more details
+
 import openpyxl
 
 #Open a file in py and import CO data without the NULL DATA 
@@ -9,8 +9,8 @@ sheet = workbook.active
 
 
 
-#Delete empty rows
-#The method is repeated becaues the method doesn't delete 2 empty rows in order
+#Deleting empty rows
+#The method was repeated becaues the method didn't delete 2 or more empty rows next to each other
 for row in range (2,6415): 
     cell = sheet['J' + str (row)].value
     if cell == None:
